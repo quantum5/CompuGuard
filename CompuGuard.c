0d7f2b36a67fe0919f485c7beb7a80f188b840f8
Xiaomao Chen <xiaomao5@live.com>
2012-10-18 15:09:34 -0400

    Initial Commit

---
+++ CompuGuard.c
@@ -0,0 +1,5 @@
+#include <windows.h>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	MessageBox(NULL, "Hello World!", "Hello", MB_ICONINFORMATION);
+}
